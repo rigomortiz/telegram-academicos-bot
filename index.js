d6c6e5e888de470093fc1b05885cd0e199a2f470
Rigoberto Canseco <rigoberto.canseco.contractor@bbva.com>
2022-11-14 17:08:29 -0600

    add bot demo

--- index.js
+++ index.js
@@ -1,21 +1,93 @@
-const http = require('http');
-const TelegramBot = require('node-telegram-bot-api')
-const API_KEY = process.env.API_KEY
+require('dotenv').config()
+const express = require('express')
+const axios = require('axios')
+const crypto = require("crypto")
 
-let bot = new TelegramBot(API_KEY, { polling: true });
+const { PORT, API_KEY, SERVER_URL, SECRET_MD5, GOOGLE_FORMS } = process.env
 
-// Create a bot that uses 'polling' to
-// fetch new updates
-bot.on("polling_error", (err) => console.log(err));
+// app initialization
+const app = express()
 
-// The 'msg' is the received Message from user and
-// 'match' is the result of execution above
-// on the text content
-bot.onText(/\/start/, function (msg, match) {
-    bot.sendMessage(msg.chat.id, "Hey, I'm a brand new Telegram bot. I live inside a Sanity tutorial.");
-});
+// middlewares
+app.use(express.json())
 
-bot.on('message', (msg) => {
-    console.log(msg);
-  //   bot.sendMessage(msg.chat.id, "Message logged to console!")
+// Telegram API Configuration
+const TELEGRAM_API = `https://api.telegram.org/bot${API_KEY}`
+const URI = `/webhook/${API_KEY}`
+const webhookURL = `${SERVER_URL}${URI}`
+
+// configuring the bot via Telegram API to use our route below as webhook
+const setupWebhook = async () => {
+    try {
+        const { data } = await axios.get(`${TELEGRAM_API}/setWebhook?url=${webhookURL}&drop_pending_updates=true`)
+        console.log(`setupWebhook:`, data)
+    } catch (error) {
+        console.log(`Error:`, error)
+        return error
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+    console.log('Hello World!')
+})
+
+
+app.listen(PORT, async () => {
+    // setting up our webhook url on server spinup
+    try {
+        console.log(`Server is up and Running at PORT : ${PORT}`)
+        await setupWebhook()
+    } catch (error) {
+        console.log(error.message)
+    }
+})
+
+app.post(URI,  async (req, res) => {
+    try {
+        const {message} = req.body
+        const username = message.from.username || ''
+
+        // console.log(`message:`, message)
+        // console.log(`Message from ${username}: ${message.text}`)
+
+        if (message.text === '/start') {
+            // Typing chat action
+            await axios.post(`${TELEGRAM_API}/sendChatAction`, {
+                chat_id: message.chat.id,
+                action: 'typing'
+            })
+
+            // Sleep for 2 seconds
+            await new Promise(resolve => setTimeout(resolve, 2000))
+
+            // Send message
+            await axios.post(`${TELEGRAM_API}/sendMessage`, {
+                chat_id: message.chat.id,
+                text: `¡Hola ${username}!\nPara ingresar al grupo escribe:\n /ingresar`
+            })
+        } else if(message.text === '/ingresar') {
+            // Typing chat action
+            await axios.post(`${TELEGRAM_API}/sendChatAction`, {
+                chat_id: message.chat.id,
+                action: 'typing'
+            })
+            // MD5 Hash
+            const MD5 = crypto.createHmac("md5", SECRET_MD5).update(message.from.id.toString()).digest("hex")
+            // Sleep for 2 seconds
+            await new Promise(resolve => setTimeout(resolve, 2000))
+            // Send message
+            await axios.post(`${TELEGRAM_API}/sendMessage`, {
+                chat_id: message.chat.id,
+                text: `Para ingresar al grupo es necesario que cuentes con correo @ciencias.
+                \n1. Llena el siguiente formulario ${GOOGLE_FORMS + MD5 + ',' + message.from.id + ',' + username}
+                \n2. Espera el correo de confirmación y el link por este chat.
+                \n3. Si tienes problemas para ingresar envía un mensaje a @rigomortiz.`
+            })
+        }
+
+        res.status(200).send('ok');
+    } catch (error) {
+        console.log(error.message)
+    }
 })
